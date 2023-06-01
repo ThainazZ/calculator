dd542814627a24f72bcf2b7f13fc167c71d6828a
ThainazZ <thaina.dias@grendene.com.br>
2023-06-01 14:09:17 -0300

    seconde commit

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+import './App.css';
+
 
 const App = () => {
   const [result, setResult] = useState("");
+
+    const handleClick = (e) => {
+        setResult(result.concat(e.target.name));
+    }
+
   return (
     <>
       <div className="container">
@@ -9,7 +16,28 @@ const App = () => {
           <input type="text" value={result} />
         </form>
 
-        <div className="keypad"></div>
+        <div className="keypad">
+            <button onClick={handleClick}>AC</button>
+            <button onClick={handleClick}>%</button>
+            <button name onClick={handleClick}>&divide;</button>
+            <button onClick={handleClick}>DEL</button>
+            <button onClick={handleClick}>7</button>
+            <button onClick={handleClick}>8</button>
+            <button onClick={handleClick}>9</button>
+            <button onClick={handleClick}>&times;</button>
+            <button onClick={handleClick}>4</button>
+            <button onClick={handleClick}>5</button>
+            <button onClick={handleClick}>6</button>
+            <button onClick={handleClick}>&ndash;</button>
+            <button onClick={handleClick}>1</button>
+            <button onClick={handleClick}>2</button>
+            <button onClick={handleClick}>3</button>
+            <button onClick={handleClick}>+</button>
+            <button onClick={handleClick}>C</button>
+            <button onClick={handleClick}>0</button>
+            <button onClick={handleClick}>,</button>
+            <button onClick={handleClick}>=</button>
+            </div>
       </div>
     </>
   );
