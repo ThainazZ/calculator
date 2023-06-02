205a0379ce395811e1e493b79d1654d0e435d233
ThainazZ <thaina.dias@grendene.com.br>
2023-06-02 12:40:34 -0300

    third commit - quase tudo funcional

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App';
 
-ReactDOM.render(
+ReactDOM.render (
 <React.StrictMode>
 <App />
 </React.StrictMode>, 
